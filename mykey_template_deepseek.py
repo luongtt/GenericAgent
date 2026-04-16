47a6bea251c5c0abcd30531edd66382aa0e70449
luongtt <28631182+luongtt@users.noreply.github.com>
2026-04-16 16:22:18 +0700

    key temp deepseek model

---
+++ mykey_template_deepseek.py
@@ -0,0 +1,15 @@
+
+native_oai_config = {
+    'name': 'deepseek',                
+    'apikey': 'sk-xxx',  
+    'apibase': 'https://api.deepseek.com/v1',
+    'model': 'deepseek-reasoner',         
+    'max_retries': 2,                  
+    'connect_timeout': 10,             
+    'read_timeout': 120,               
+}
+
+tg_bot_token = ''
+tg_allowed_users = []
+
+proxy = None
